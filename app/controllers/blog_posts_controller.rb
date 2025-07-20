13b38c27c883ce7ce213a2421d15ed4bb1f34bbf
William-nyarash <owetywilliamnyaranga@gmail.com>
2025-07-20 17:56:20 +0300

    modify redundant code

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,11 +1,10 @@
 class BlogPostsController < ApplicationController
-   # before_action :set_blog_post, expe
+   before_action :set_blog_post, except: [ :index, :new, :create ]
    def index
       @blog_posts = BlogPost.all
    end
 
    def show
-      @blog_post = BlogPost.find(params[:id])
    rescue ActiveRecord::RecordNotFound
       redirect_to root_path
    end
@@ -25,13 +24,11 @@ class BlogPostsController < ApplicationController
 
 
    def edit
-      @blog_post = BlogPost.find(params[:id])
    rescue
       redirect_to root_path
    end
 
    def update
-      @blog_post = BlogPost.find(params[:id])
       if @blog_post.update(blog_post_params)
         redirect_to @blog_post
       else
@@ -40,7 +37,6 @@ class BlogPostsController < ApplicationController
    end
 
    def destroy
-         @blog_post = BlogPost.find(params[:id])
          @blog_post.destroy
          redirect_to root_path
    end
@@ -49,5 +45,9 @@ class BlogPostsController < ApplicationController
 
    def blog_post_params
      params.require(:blog_post).permit(:title, :body)
+   end
+
+   def set_blog_post
+     @blog_post = BlogPost.find(params[:id])
    end
 end
